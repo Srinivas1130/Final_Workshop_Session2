455e63e18d158d104c40814b2046868c6934e52d
Srinivas1130 <srinivasachar1998@gmail.com>
2022-06-27 12:48:38 +0530

    Added:Ability to add and delete the contact from the address list

---
+++ AddressBookSite.js
@@ -0,0 +1,5 @@
+const site_properties = {
+    home_page: "C:\\Users\\Admin\\eclipse-workspace\\BridgeLabz\\Day_38\\AddressBook2.html",
+    add_contacts_page: "C:\\Users\\Admin\\eclipse-workspace\\BridgeLabz\\Day_38\\AddressBook.html",
+    home_page: "C:\\Users\\Admin\\eclipse-workspace\\BridgeLabz\\Day_38\\AddressBook2.html"
+  };
